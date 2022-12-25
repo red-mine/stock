80a436f7f2d15cec5248f920678d4efb810b27ac
林恒 <hunt.lin@outlook.com>
2022-12-25 11:12:14 +0800

    database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # get 'stocks/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/stocks", to: "stocks#index"
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "stocks#index"
 end
