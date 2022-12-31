9b0913694b8e80cacc5b797a2eddb3016147dabb
Hunt Lin <hunt.lin@tes-tec.com>
2022-12-31 08:31:36 +0800

    On server

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_25_025834) do
-  create_table "stocks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "stocks", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "code"
     t.date "date"
     t.float "price"
