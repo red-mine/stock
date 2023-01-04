d46dcc062b00d6f0cf5536b7d91f37f03b0e3134
林恒 <hunt.lin@outlook.com>
2023-01-04 22:28:29 +0800

    Good Progress.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -11,6 +11,10 @@ class StocksController < ApplicationController
     @stock_mu100  = stock.good_data_updn(params[:stock], 100, true)
     @stock_md100  = stock.good_data_updn(params[:stock], 100, false)
     @stave_trend  = stock.good_data_trend(params[:stock])
+    @stave_up1    = stock.good_stave_updn(params[:stock], true,  1)
+    @stave_dn1    = stock.good_stave_updn(params[:stock], false, 1)
+    @stave_up2    = stock.good_stave_updn(params[:stock], true,  2)
+    @stave_dn2    = stock.good_stave_updn(params[:stock], false, 2)
     @stocks = [
       {name: "ma10",  data: @stock_ma10}, 
       {name: "ma100", data: @stock_ma100},
@@ -19,7 +23,11 @@ class StocksController < ApplicationController
     ]
     @staves = [
       {name: "ma10",  data: @stock_ma10}, 
-      {name: "trend", data: @stave_trend}
+      {name: "trend", data: @stave_trend},
+      {name: "up1",   data: @stave_up1},
+      {name: "dn1",   data: @stave_dn1},
+      {name: "up2",   data: @stave_up2},
+      {name: "dn2",   data: @stave_dn2}
     ]
     @stock_code = params[:stock]
   end
