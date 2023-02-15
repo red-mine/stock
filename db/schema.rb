e1901f54f0f012ce2693837f7a3bef434b627436
林恒 <hunt.lin@outlook.com>
2023-02-15 19:50:50 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -19,15 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_062603) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "stocks_coef_years", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "stock"
-    t.float "coef"
-    t.float "inter"
-    t.float "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "stocks_coefs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "stock"
     t.float "coef"
