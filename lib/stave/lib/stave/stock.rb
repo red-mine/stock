b3256d61b74a6d5a9b5d8d39ed6b413a53c9ea01
Your Name <Hunt Redmine>
2023-03-02 11:07:08 +0800

    commit

--- lib/stave/lib/stave/stock.rb
+++ lib/stave/lib/stave/stock.rb
@@ -13,7 +13,7 @@ module Stave
       puts "Modeling..."
       good_stocks.with_progress do |good_stock|
         Progress.note = good_stock.upcase
-        good_model = _good_model(good_stock)
+        good_model  = _good_model(good_stock)
         if good_model.empty?
           next
         end
@@ -26,30 +26,22 @@ module Stave
       good_table.delete_all
       puts "Staveing..."
       @good_models.with_progress do |good_model|
+        Progress.note = good_model[:stock].upcase
         good_stock  = good_model[:stock]
         good_last   = good_model[:price]
         good_date   = good_model[:date]
-
-        Progress.note = good_stock.upcase
-
         good_boll   = good_aver(good_stock, STAVE)
         good_boll   = good_boll[-1][1]
-
         good_trend  = good_trend(good_stock)
         good_trend  = good_trend[-1][1]
-
         good_up1    = good_stave(good_stock, true, 1) # up1
         good_up1    = good_up1[-1][1]
-
         good_top    = good_stave(good_stock, true, 2) # up2
         good_top    = good_top[-1][1]
-
-        good_price = good_last > good_trend && 
-                     good_last > good_boll && 
-                     good_last > good_up1
-
-        good_stave = good_last > good_top
-
+        good_price  = good_last > good_trend && 
+                      good_last > good_boll && 
+                      good_last > good_up1
+        good_stave  = good_last > good_top
         if good_price
           good_stock = good_table.new(
             stock:  good_stock, 
@@ -66,22 +58,22 @@ module Stave
     end
 
     def good_aver(good_stock, good_days)
-      good_aver = _good_aver(good_stock, good_days)
-      good_start = good_days - 1
-      good_end = good_aver.size - 1
-      good_aver = good_aver.slice!(good_start, good_end - good_start + 1)
+      good_aver   = _good_aver(good_stock, good_days)
+      good_start  = good_days - 1
+      good_end    = good_aver.size - 1
+      good_aver   = good_aver.slice!(good_start, good_end - good_start + 1)
       good_aver
     end
 
     def good_trend(good_stock)
       good_trend  = _good_trend(good_stock)
-      good_trend = good_trend.pluck(:date, :price)
+      good_trend  = good_trend.pluck(:date, :price)
       good_trend
     end
 
     def good_stave(good_stock, good_stave, good_multi)
-      good_data = good_trend(good_stock)
-      good_sqrt = _good_sqrt(good_stock)
+      good_data   = good_trend(good_stock)
+      good_sqrt   = _good_sqrt(good_stock)
       if good_stave
         good_data.map! { |good_date, good_price| [good_date, good_price + good_sqrt * good_multi] }
       else
@@ -90,10 +82,10 @@ module Stave
     end
 
     def good_boll(good_stock, good_days, good_boll)
-      good_aver = _good_aver(good_stock, good_days)
-      good_sqrt = _good_boll(good_stock, good_days)
-      good_start = good_aver.size - good_sqrt.size
-      good_end = good_aver.size - 1
+      good_aver   = _good_aver(good_stock, good_days)
+      good_sqrt   = _good_boll(good_stock, good_days)
+      good_start  = good_aver.size - good_sqrt.size
+      good_end    = good_aver.size - 1
       for good_index in good_start..good_end
         if good_boll
           good_aver[good_index][1] += 2 * good_sqrt[good_index - good_start]
@@ -101,24 +93,24 @@ module Stave
           good_aver[good_index][1] -= 2 * good_sqrt[good_index - good_start]
         end
       end
-      good_aver = good_aver.slice!(good_start, good_end - good_start + 1)
+      good_aver   = good_aver.slice!(good_start, good_end - good_start + 1)
       good_aver
     end
 
     private
 
     def _good_move(good_price, good_days)
-      good_move = good_price.each_cons(good_days).map { 
+      good_move   = good_price.each_cons(good_days).map { 
         |good_aver| good_aver.reduce(&:+).fdiv(good_days).round(2) 
       }
     end
 
     def _good_aver(good_stock, good_days)
-      good_data = _good_data(good_stock)
-      good_price = good_data.pluck(:price)
-      good_aver = _good_move(good_price, good_days)
-      good_start = good_price.size - good_aver.size
-      good_end = good_price.size - 1
+      good_data   = _good_data(good_stock)
+      good_price  = good_data.pluck(:price)
+      good_aver   = _good_move(good_price, good_days)
+      good_start  = good_price.size - good_aver.size
+      good_end    = good_price.size - 1
       for good_index in good_start..good_end
         good_data[good_index][:price] = good_aver[good_index - good_start]
       end
@@ -126,8 +118,8 @@ module Stave
     end
 
     def _good_dist(good_stock, good_days)
-      good_data = _good_days(good_stock, good_days).pluck(:date, :price)
-      good_aver = _good_aver(good_stock, good_days)
+      good_data   = _good_days(good_stock, good_days).pluck(:date, :price)
+      good_aver   = _good_aver(good_stock, good_days)
       good_data.each_with_index do |good_data_, good_index|
         good_data[good_index][1] = good_data_[1] - good_aver[good_index][1]
         good_data[good_index][1] = good_data[good_index][1] ** 2
@@ -136,9 +128,9 @@ module Stave
     end
 
     def _good_boll(good_stock, good_days)
-      good_dist = _good_dist(good_stock, good_days)
+      good_dist   = _good_dist(good_stock, good_days)
       good_dist.map! { |good_date, good_price| good_price }
-      good_sqrt = _good_move(good_dist, good_days)
+      good_sqrt   = _good_move(good_dist, good_days)
       good_sqrt.each_with_index do |good_data, good_index|
         good_sqrt[good_index] = Math.sqrt(good_data)
       end
@@ -146,10 +138,10 @@ module Stave
     end
     
     def _good_days(good_stock, good_days)
-      good_data = _good_data(good_stock)
-      good_start = STAVE - good_days
-      good_end = good_data.size - 1
-      good_data = good_data.slice!(good_start, good_end - good_start + 1)
+      good_data   = _good_data(good_stock)
+      good_start  = STAVE - good_days
+      good_end    = good_data.size - 1
+      good_data   = good_data.slice!(good_start, good_end - good_start + 1)
       good_data
     end
 
@@ -164,39 +156,43 @@ module Stave
     def _good_stock(good_file, good_index)
       good_binary = good_file.read(32)
       good_string = good_binary.unpack("H*")[0]
-      good_date = good_string[0,8]
-      good_date = good_date[6..7] + good_date[4..5] + good_date[2..3] + good_date[0..1]
-      good_date = good_date.to_i(16).to_s
-      good_price = good_string[32,8]
-      good_price = good_price[6..7] + good_price[4..5] + good_price[2..3] + good_price[0..1]
-      good_price = good_price.to_i(16).to_f / STAVE
-      good_stock = {date: good_date.to_date, price: good_price, index: good_index}
+      good_date   = good_string[0,8]
+      good_date   = good_date[6..7] + good_date[4..5] + good_date[2..3] + good_date[0..1]
+      good_date   = good_date.to_i(16).to_s
+      good_price  = good_string[32,8]
+      good_price  = good_price[6..7] + good_price[4..5] + good_price[2..3] + good_price[0..1]
+      good_price  = good_price.to_i(16).to_f / STAVE
+      good_stock  = {
+        date:   good_date.to_date, 
+        price:  good_price, 
+        index:  good_index
+      }
       good_stock
     end
 
     def _good_data(good_stock) 
-      good_file = _good_file(good_stock)
+      good_file   = _good_file(good_stock)
       if good_file.nil?
         return []
       end
-      good_data = []
-      good_index = 0
+      good_data   = []
+      good_index  = 0
       good_file.pos = good_file.size - 1 * 32
-      good_stock = _good_stock(good_file, good_index)
+      good_stock  = _good_stock(good_file, good_index)
       if good_stock[:price] > STAVE
         return []
       end
       good_file.pos = good_file.size - @good_days * 32
       while good_file.eof == false
-        good_stock = _good_stock(good_file, good_index)
+        good_stock  = _good_stock(good_file, good_index)
         good_data.push good_stock
-        good_index += 1
+        good_index  += 1
       end
       good_data
     end
 
     def _good_model(good_stock)
-      good_data = _good_data(good_stock)
+      good_data   = _good_data(good_stock)
       if good_data.empty?
         return {}
       end
@@ -205,7 +201,7 @@ module Stave
       end
       good_index  = good_data.pluck(:index)
       good_price  = good_data.pluck(:price)
-      good_date   = good_data.pluck(:date)
+      good_date   = good_data.pluck(:date)     
       good_model  = Eps::LinearRegression.new(good_index, good_price, split: false)
       good_coef   = good_model.coefficients[:x0]
       if good_coef < 1.0 / STAVE
@@ -224,8 +220,8 @@ module Stave
     end
 
     def _good_trend(good_stock)
-      good_stave = _good_stave(good_stock)
-      good_model = _good_model(good_stock)
+      good_stave  = _good_stave(good_stock)
+      good_model  = _good_model(good_stock)
       good_stave.each_with_index do |good_data, good_index|
         good_stave[good_index][:price] = good_model[:coef] * good_index + good_model[:inter]
       end
@@ -233,8 +229,8 @@ module Stave
     end
 
     def _good_sqrt(good_stock)
-      good_stave = _good_stave(good_stock)
-      good_trend = _good_trend(good_stock)
+      good_stave  = _good_stave(good_stock)
+      good_trend  = _good_trend(good_stock)
       good_stave.each_with_index do |good_data, good_index|
         good_stave[good_index][:price] = good_stave[good_index][:price] - good_trend[good_index][:price]
         good_stave[good_index][:price] = good_stave[good_index][:price] ** 2
@@ -247,7 +243,7 @@ module Stave
     end
 
     def _good_stocks
-      good_files = _good_files
+      good_files  = _good_files
       good_stocks = []
       good_files.each do |good_file|
         good_stock = good_file[0,8]
@@ -257,7 +253,7 @@ module Stave
     end
 
     def _good_files
-      good_files = Dir.entries(_good_base)
+      good_files  = Dir.entries(_good_base)
       good_files.delete(".")
       good_files.delete("..")
       good_files.delete_if{ |good_file| _good_file(good_file[0,8]).nil? }
@@ -265,8 +261,8 @@ module Stave
     end
 
     def _good_file(good_stock)
-      good_path = _good_path(good_stock)
-      good_file = nil
+      good_path   = _good_path(good_stock)
+      good_file   = nil
       if File.size(good_path) > @good_days * 32
         good_file = File.open(good_path)
       end
@@ -274,14 +270,13 @@ module Stave
     end
 
     def _good_path(good_stock)
-      good_path = _good_base + good_stock + ".day"
+      good_path   = _good_base + good_stock + ".day"
       good_path
     end
 
     def _good_base
-      good_base = "storage/vipdoc/" + @good_area + "/lday/"
+      good_base   = "storage/vipdoc/" + @good_area + "/lday/"
       good_base
     end
-
   end
 end
