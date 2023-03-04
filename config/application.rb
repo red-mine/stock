67c7959c5b1d70d0fe6354b411c496fd2154ff1b
Hunt Redmine <hunt@red-mine.com>
2023-03-04 14:50:14 +0800

    commit

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 require "stave"
+# require "stock"
 
 module Stock
   class Application < Rails::Application
@@ -18,7 +19,7 @@ module Stock
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Central Time (US & Canada)"
-    config.eager_load_paths << Rails.root.join("extras")
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
