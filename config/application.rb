96cf9591f2865393d2977cdb350e219a195043ed
Hunt Redmine <hunt@red-mine.com>
2023-03-04 11:04:01 +0800

    commit

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "stave"
+
 module Stock
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
