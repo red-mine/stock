dabfabcdb8f2ddcf69a5b7ffd842861be764dea1
Hunt Redmine <hunt@red-mine.com>
2023-03-05 13:06:04 +0800

    change

--- storage/stave/sz/lday/sz003019.rb
+++
@@ -1,6 +0,0 @@
-file = File.open("sz003019.day")
-file_data = file.read(32)
-string = file_data.unpack("H*")[0]
-date = string[0,8]
-real_date = date[6..7] + date[4..5] + date[2..3] + date[0..1]
-puts real_date.to_i(16)
