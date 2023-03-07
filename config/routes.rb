dd8fe1e8a613019a49364c798ea314e23dcde9ea
Hunt Redmine <hunt@red-mine.com>
2023-03-07 20:03:08 +0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount Stave::Engine => "/stave"
 
   root  "stocks#index"
-
   get   "/stocks/:years",         to: "stocks#index"
+
+  get   "/stocks/:stock",         to: "stocks#show"
   get   "/stocks/:stock/:years",  to: "stocks#show"
 end
