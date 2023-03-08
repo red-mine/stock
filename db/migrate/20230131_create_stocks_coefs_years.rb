480338e9304286b52db8626cc4b2e2647d292066
Your Name <Hunt Redmine>
2023-03-08 13:51:09 +0800

    commit

--- db/migrate/20230131_create_stocks_coefs_years.rb
+++ db/migrate/20230131_create_stocks_coefs_years.rb
@@ -2,12 +2,16 @@ class CreateStocksCoefsYears < ActiveRecord::Migration[7.0]
   def change
     create_table :stocks_coefs_years do |t|
       t.string  :stock
+
       t.float   :coef
       t.float   :inter
+      
       t.float   :price
+      
       t.boolean :good
-      t.string  :stave
 
+      t.string  :stave
+      
       t.integer :boll
       t.integer :stav
       
