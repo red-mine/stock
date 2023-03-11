45fa05df644066c2f34ba8835e3d000c714d0b67
Hunt Redmine <hunt@red-mine.com>
2023-03-11 22:33:55 +0800

    commit

--- db/migrate/20230312_create_stave_staves.rb
+++ db/migrate/20230312_create_stave_staves.rb
@@ -1,7 +1,7 @@
 # This migration comes from stave (originally 20230311)
-class CreateStavesStavs < ActiveRecord::Migration[7.0]
+class CreateStaveStaves < ActiveRecord::Migration[7.0]
   def change
-    create_table :staves_stavs do |t|
+    create_table :stave_staves do |t|
       t.string :stock
       t.float :price
       t.date :date
