1be8e5a83bade333d7ad515fcfd63b8fec1b6455
Hunt Redmine <hunt@red-mine.com>
2023-03-14 11:08:18 +0800

    commit

--- config/application.rb
+++ config/application.rb
@@ -7,19 +7,30 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 require "stave"
-# require "stock"
+require "stock"
 
-module Stock
+module StockApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Custom directories with classes and modules you want to be autoloadable.
+    config.autoloader = :zeitwerk
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Central Time (US & Canada)"
+    config.eager_load_paths << Rails.root.join("extras")
+
+    config.eager_load_paths += %W(
+      #{config.root}/lib/stock
+      #{config.root}/lib/stave
+    )
+
+    config.autoload_paths << Rails.root.join("extras")
+    config.add_autoload_paths_to_load_path = true
   end
 end
---
+++ extras/stock.rb
@@ -0,0 +1,8 @@
+module Stave
+  class Stock
+    def initialize(good_area, good_years)
+      @good_area    = good_area
+      @good_years   = good_years
+    end
+  end
+end
